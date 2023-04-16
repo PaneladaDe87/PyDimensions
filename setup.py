1def0a44c100c81b6029e840b527063850fc71b5
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-04-16 08:57:19 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyDimensions',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Lista de dependências do seu módulo
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
