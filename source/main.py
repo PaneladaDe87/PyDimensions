960651e6b6fcf7f9e0e88e769cb4d37486bbf958
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-04-16 00:40:56 -0300

    Create main.py

---
+++ source/main.py
@@ -0,0 +1,12 @@
+class Camera:
+    def __init__(self, id, coord, angle):
+        super().__init__()
+
+        self.id = id
+        self.coord = coord
+        self.angle = angle
+
+    def move(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
